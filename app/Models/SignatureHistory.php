b0c7809090feb3da4e8f815e6e8a7bf0a6ff0524
Carlos <carrlosmr@gmail.com>
2024-07-02 17:47:36 -0300

    feat: defininf model attributes

--- app/Models/SignatureHistory.php
+++ app/Models/SignatureHistory.php
@@ -2,10 +2,23 @@
 
 namespace App\Models;
 
+use App\Enums\SignatureStatus;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class SignatureHistory extends Model
 {
     use HasFactory;
+
+    public $timestamps = false;
+    protected $fillable = [
+        'signature_id',
+        'last_plan_id',
+        'last_status'
+    ];
+
+    protected $casts = [
+        'last_status' => SignatureStatus::class
+    ];
+
 }
--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -2,10 +2,23 @@
 
 namespace App\Models;
 
+use App\Enums\TransactionStatus;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Transaction extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'signature_id',
+        'total_price',
+        'status',
+    ];
+
+    protected $casts = [
+        'status' => TransactionStatus::class
+    ];
+
 }
